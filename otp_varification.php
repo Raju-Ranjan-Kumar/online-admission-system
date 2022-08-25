b82c1e65d9c00dbf5baba258f8359fbeb9eb100f
raju ranjan <rajuranjansimari110@gmai.com>
2022-08-25 13:06:41 +0530

    raju

--- otp_varification.php
+++ otp_varification.php
@@ -4,17 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>otp varification</title>
+    <!-- Bootstarp 5.2.0 CSS $ JS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <title>Otp varification</title>
     <style>
         *{padding:0; margin:0;}
         body {background:#eee;}
         .container{height:100vh; display:flex; justify-content:center; align-items:center; text-align:center;}
-        .card {width:350px; padding:10px; border-radius:20px; background:#fff; border:none; height:400px; position:relative;}
-        .heading{font-size:25px; padding:25px; text-transform:uppercase;}
-        .mobile-text {color:#989696b8; font-size:20px;}
-        .take-input{padding-top:45px;}
-        .control {height:32px; width:200px; border-radius:7px;}
-        .sub-btn{height:35px; width:100px; margin-top:35px; background-color:green; color:white; font-weight:bold; border-radius:12px;}
+        .card {width:350px; padding:25px; border-radius:20px; background:#fff; border:none; position:relative;}
+        .heading{font-size:25px; padding:15px; text-transform:uppercase;}
+        .mobile-text {color:#989696b8; font-size:18px;}
         .resend{padding-top:30px}
         .cursor{cursor:pointer; font-weight:bold; font-size:20px; color:red;}
     </style>
@@ -89,14 +89,20 @@
         <form action="" method="post">
             <div class="card">
                 <h5 class="heading">Otp Verification</h5>
-                <span class="mobile-text">Enter the otp we just send on your <b class="">Email</b></span>
-                <div class="take-input">
-                    <input type="email" class="control" name="email" placeholder="Your email"><br><br>
-                    <input type="number" class="control" name="ve_otp" placeholder="Enter otp">
-                </div>
-                <div class="">
-                    <button type="submit" name="submit" class="sub-btn">Submit</button>
-                </div>
+                <span class="mobile-text">Enter the otp we just send on your <b class="text-primary">Email</b></span><br>
+
+                <div class="input-group flex-nowrap">
+                    <span class="input-group-text" id="addon-wrapping">@</span>
+                    <input type="email" name="email" class="form-control" placeholder="Useremail" aria-label="Useremail" aria-describedby="addon-wrapping">
+                </div><br>
+
+                <div class="input-group flex-nowrap">
+                    <span class="input-group-text" id="addon-wrapping">$</span>
+                    <input type="text" name="ve_otp" class="form-control" placeholder="Enter otp" aria-describedby="addon-wrapping">
+                </div><br>
+
+                <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+
                 <div class="resend">
                     <span class="mobile-text">Don't receive the code?</span>
                     <span class="cursor">Resend</span>
